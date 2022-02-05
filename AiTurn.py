57f896e2a53e5e346e91e5c4122a5f048e2661ae
Michael Chalupiak <mikecchalupiak@outlook.com>
2022-02-05 18:15:03 -0500

    Completely functional

--- AiTurn.py
+++ AiTurn.py
@@ -1,12 +1,17 @@
 import random as rd
 
 def aigo(list):
+    moves = []
     while 1 == 1:
         computerMove = rd.randint(0,8)
         if list[computerMove] == " ":
             break
 
         else:
+            if computerMove not in moves:
+                moves.append(computerMove)
+            if len(moves) >= 9:
+                return 9
             continue
 
     return computerMove
--- TTT.py
+++ TTT.py
@@ -3,10 +3,17 @@ from GameBoard import gameboard
 from AiTurn import aigo
 from time import sleep
 
+def hasWinner():
+    for i in range(len(winningMoves)):
+        if gameSpaces[winningMoves[i][0]] == gameSpaces[winningMoves[i][1]] == gameSpaces[winningMoves[i][2]] and " " not in [gameSpaces[winningMoves[i][0]], gameSpaces[winningMoves[i][1]], gameSpaces[winningMoves[i][2]]]:
+            return True
+    return False
+
 incorrectPiece = 0
 on = True
 startboard() ; sleep(.5)
 gameSpaces = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+winningMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [3, 5, 8], [0, 4, 8], [2, 4, 6]]
 while on:
     userPiece = input("Now, what piece do you want to be, X or O? ")
     if incorrectPiece > 5:
@@ -32,11 +39,24 @@ while on:
     userMove = input("What spot do you want to place a piece on? Ex: 1, 2, 3, etc ")
     if gameSpaces[int(userMove)-1] == " ":
         gameSpaces[int(userMove) - 1] = userPiece.upper()
+        if hasWinner():
+            gameboard(gameSpaces[0], gameSpaces[1], gameSpaces[2], gameSpaces[3], gameSpaces[4], gameSpaces[5], gameSpaces[6], gameSpaces[7], gameSpaces[8])
+            print("You won!")
+            break
     
     else:
         print("Sorry, that space is already taken")
         continue
     
-    gameSpaces[aigo(gameSpaces)] = aiPiece
+    computerMove = aigo(gameSpaces)
+    if computerMove != 9:
+        gameSpaces[computerMove] = aiPiece
+        if hasWinner():
+            gameboard(gameSpaces[0], gameSpaces[1], gameSpaces[2], gameSpaces[3], gameSpaces[4], gameSpaces[5], gameSpaces[6], gameSpaces[7], gameSpaces[8])
+            print("You lost!")
+            break
+    else:
+        print("Board full... Game is over")
+        on = False
     gameboard(gameSpaces[0], gameSpaces[1], gameSpaces[2], gameSpaces[3], gameSpaces[4], gameSpaces[5], gameSpaces[6], gameSpaces[7], gameSpaces[8])
-    
+
